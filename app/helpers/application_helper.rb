334597a33d751f31a3150a415a50514393a96131
subhashsaran <subhashthehacker@gmail.com>
2015-10-27 17:53:18 +0530

    if user already have image then use that else gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
   def avatar_url(user)
+  	## this is section if users image is present 
     if user.avatar_url.present?
       user.avatar_url
+  	## if user don't have a image already user gravatar
     else
       default_url = "#{root_url}images/guest.png"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
